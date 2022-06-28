87057bd2e553836169e084630e6c52a7efab1f76
Roelvis Coello <rcabijana@gmail.com>
2022-06-27 21:39:50 -0400

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+using System.Windows.Forms;
+
+namespace mergeLists
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static SinglyLinkedListNode mergeLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2)
+        {
+            if (head1 == null) return head2;
+            if (head2 == null) return head1;
+
+            if (head1.data < head2.data)
+            {
+                head1.next = mergeLists(head1.next, head2);
+                return head1;
+            }
+            else
+            {
+                head2.next = mergeLists(head2.next, head1);
+                return head2;
+            }
+
+        }
+        static void PrintSinglyLinkedList(SinglyLinkedListNode node, string sep, TextWriter textWriter)
+        {
+            while (node != null)
+            {
+                textWriter.Write(node.data);
+
+                node = node.next;
+
+                if (node != null)
+                {
+                    textWriter.Write(sep);
+                }
+            }
+        }
+    }
+    class SinglyLinkedListNode
+    {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData)
+        {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    class SinglyLinkedList
+    {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList()
+        {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void InsertNode(int nodeData)
+        {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+}
